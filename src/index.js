159e49c3fd7774283c043dd2569ed8f746689bfc
githubschman <sarahhubschmail@gmail.com>
2018-03-27 22:45:39 -0400

    added one trophy

--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@ import 'aframe';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
 import 'babel-polyfill';
-import {Entity, Scene} from 'aframe-react';
+import { Entity, Scene } from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {color: 'red'};
+    this.state = {
+      trophiesFused: new Set(),
+      braintree: '#fff'
+    };
   }
 
-  changeColor() {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-    this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)]
-    });
+  componentDidUpdate(prev, next) {
+
+  }
+
+  fuseTrophy = (trophyId) => {
+    console.log(trophyId)
+    this.setState({trophiesFused: this.state.trophiesFused.add(trophyId), [trophyId]: '#00a0cc'});
   }
 
   fuseTrophy() {
@@ -24,9 +29,10 @@ class App extends React.Component {
   }
 
   render () {
+    console.log(this.state.braintree)
     /* Entity Graveyard
         <Entity id="box"
-          class="clickable"
+          class="unfused"
           geometry={{primitive: 'box'}}
           material={{color: this.state.color, opacity: 0.6}}
           animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
@@ -45,20 +51,37 @@ class App extends React.Component {
       <Scene>
         <a-assets>
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
-          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
-
-          <a-mixin id="smol" scale=".1 .1 .1"></a-mixin>
-
-          <item id="crowdfave-obj" src="models/crowd-favorite.obj"></item>
-          <item id="crowdfave-mtl" src="material/crowd-favorite-clear.mtl"></item>
+          <img id="skyTexture" src="material/paper.jpg"/>
         </a-assets>
  
-        <Entity obj-model='obj: models/crowd-favorite.obj; mtl: material/crowd-favorite-clear.mtl'
-                class="clickable"
+        <Entity obj-model='obj: models/crowd-favorite.obj;'
+                material={{color: '#de7e00'}}
+                class="unfused"
                 scale="0.05 0.05 0.05" 
                 position={{x: 0, y: 0, z: -5}}
                 rotation="-90 0 0"
-                events={{fusing: this.fuseTrophy.bind(this)}}/>
+                events={{fusing: this.fuseTrophy.bind(this)}}>
+            <a-animation begin="fusing" easing="ease-in" attribute="scale" dur="1000" fill="backwards" from="3 3 3" to="0.1 0.1 0.1"></a-animation>
+        </Entity>
+        <Entity obj-model='obj: models/braintree.obj;'
+            material={{color: this.state.braintree}}
+            className="unfused"
+            scale="0.05 0.05 0.05" 
+            position={{x: 0, y: 0, z: 5}}
+            rotation="-90 90 90"
+            events={{fusing: () => this.fuseTrophy('braintree')}}>
+          <a-animation 
+            animationstart=""
+            direction="alternate-reverse"
+            begin="fusing" 
+            attribute="rotation" 
+            fill="both" 
+            easing="linear" 
+            dur="5000" 
+            to="-90 450 90"
+            repeat="10">
+          </a-animation>
+        </Entity>
 
         <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
         <Entity primitive="a-light" type="ambient" color="#445451"/>
@@ -66,7 +89,7 @@ class App extends React.Component {
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
 
         <Entity primitive="a-camera">
-          <a-cursor raycaster="objects: .clickable" fuse="true" fuse-timeout="500">
+          <a-cursor raycaster="objects: .unfused" fuse="true" fuse-timeout="10000">
             <a-animation begin="fusing" easing="ease-in" attribute="scale" dur="1000" fill="backwards" from="3 3 3" to="0.1 0.1 0.1"></a-animation>
           </a-cursor>
         </Entity>
